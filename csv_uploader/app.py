33ed0df179fdbee191a6bd067bfbbad945497f68
apiwatpsu <92732984+apiwatpsu@users.noreply.github.com>
2025-06-09 13:25:43 +0700

    Update app.py
    change app port

--- csv_uploader/app.py
+++ csv_uploader/app.py
@@ -140,7 +140,7 @@ def logout():
 # if __name__ == '__main__':
 #     app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=1881)
 
 
 
