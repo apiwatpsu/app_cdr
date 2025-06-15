727eddfaa1964c504e7c3e1670b13a1595faab9c
apiwatpsu <92732984+apiwatpsu@users.noreply.github.com>
2025-06-15 22:26:13 +0700

    add flash

--- csv_uploader/app.py
+++ csv_uploader/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from models import db, User
 from models import db, DBConfig
