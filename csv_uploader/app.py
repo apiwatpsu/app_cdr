1da44a2c8d103c4bf5330851cb03e9c53db38f1c
apiwatpsu <92732984+apiwatpsu@users.noreply.github.com>
2025-06-15 11:43:41 +0700

    fix py

--- csv_uploader/app.py
+++ csv_uploader/app.py
@@ -30,7 +30,7 @@ def login():
         user = User.query.filter_by(username=request.form['username']).first()
         if user and check_password_hash(user.password, request.form['password']):
             session['username'] = user.username
-            return redirect(url_for('report_agent_performance'))
+            return redirect(url_for('average_call_handling_by_agent'))
         else:
             return render_template('login.html', error='Invalid credentials')
     return render_template('login.html')
