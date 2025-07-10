a97ba512127890f9af40285fcf58cf3fd7e1b489
apiwatpsu <92732984+apiwatpsu@users.noreply.github.com>
2025-07-10 15:27:33 +0700

    add makecall url

--- csv_uploader/app.py
+++ csv_uploader/app.py
@@ -363,6 +363,8 @@ def system_config():
         SystemConfig.set("TCX_CLIENT_SECRET", request.form['tcx_client_secret'])
         SystemConfig.set("TCX_GRANT_TYPE", request.form['tcx_grant_type'])
         SystemConfig.set("TCX_CLIENT_ID", request.form['tcx_client_id'])
+        SystemConfig.set("TCX_MAKECALL_URL", request.form['tcx_makecall_url'])
+
 
 
         flash('Saved successfully', 'success')
@@ -377,7 +379,8 @@ def system_config():
         tcx_token_url = SystemConfig.get("TCX_TOKEN_URL", ""),
         tcx_client_secret = SystemConfig.get("TCX_CLIENT_SECRET", ""),
         tcx_grant_type = SystemConfig.get("TCX_GRANT_TYPE", ""),
-        tcx_client_id = SystemConfig.get("TCX_CLIENT_ID", "")
+        tcx_client_id = SystemConfig.get("TCX_CLIENT_ID", ""),
+        tcx_makecall_url = SystemConfig.get("TCX_MAKECALL_URL", "")
     )
 
 
