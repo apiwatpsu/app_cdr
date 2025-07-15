037ae60ec8e9a6842289b8463c4dbfea10603952
apiwatpsu <92732984+apiwatpsu@users.noreply.github.com>
2025-07-15 22:10:49 +0700

    add fileds

--- csv_uploader/models.py
+++ csv_uploader/models.py
@@ -126,6 +126,7 @@ class CampaignMessage(db.Model):
 
 class Knowledge(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
     raw_data = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
