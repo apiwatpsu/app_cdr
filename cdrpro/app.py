e0d9dab6a2642c7672dd488c14b1d5289b65dfa3
apiwatpsu <92732984+apiwatpsu@users.noreply.github.com>
2025-07-18 20:51:42 +0700

    debug

--- cdrpro/app.py
+++ cdrpro/app.py
@@ -3460,7 +3460,7 @@ def add_security_headers(response):
 
 if __name__ == '__main__':
 
-    app.run(debug=False, host='0.0.0.0', port=1881)
+    app.run(debug=True, host='0.0.0.0', port=1881)
 
 
 
